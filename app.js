12f17677e6f84a3af137f058dba6d531cbcb1599
sargsyanshahen <sargsyanshahen@gmail.com>
2021-07-18 22:37:06 +0400

    created working game logic

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const port = 3000
 global.players = [];
 global.cards = [];
 global.winner = [];
+global.game = {
+  'winner': '',
+  'players': []
+};
 
 app.use('/api/game', require('./routes/game.routes'));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,52 +1,72 @@
 const {Router} = require('express')
 //const config = require('config')
 const router = Router()
-//const https = require('https');
 const request = require("request");
-
 
 function getCards(url) {
     return new Promise(function (resolve, reject) {
         request(url, function (error, res, body) {
-        if (!error && res.statusCode == 200) {
-            resolve(JSON.parse(body));
-        } else {
-            reject(error);
-        }
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                reject(error);
+            }
         });
     });
 }
 
 async function main(url) {
     let res = await getCards(url);
-    console.log('res',res);
     return res;
 }
 
 function addPlayer(name) {
-    players.push({
+    let player = {
         'name' : name,
         'points' : 0,
         'cards' : [],
-    });
+    }
+    players.push(player);
+    game.players.push(player)
 }
 
 function resetPlayers(name) {
     players = [];
+    game.players = [];
 }
 
 function play() {
     players.forEach((player, index) => {
         pullTheCard(index, 2)
     });
-    //checkWinner()
+    
+    if(winnerPlayer = checkBlackjackWinner()) {
+        game['winner'] = winnerPlayer['name'];
+        return;
+    }
+    
+    players.forEach((player, index) => {
+        do {
+            pullTheCard(index);
+            if (players[index].points > 21) {
+                players.splice(index, 1);
+                break;
+            }
+        }
+        while (players[index].points <= 17);
+
+        if (players.length == 1) {
+            return;
+        }
+    });
+    
     
 }
 
 function pullTheCard(index, count = 1) {
     for(i=0; i<count; i++) {
         let card = cards.shift();
-        //console.log(players[index],cards)
+        console.log('players[index]',players[index],card)
         players[index].points+=getCardPoints(card.value)
         players[index].cards.push(getCardName(card))
     }
@@ -54,7 +74,7 @@ function pullTheCard(index, count = 1) {
 
 function getCardPoints(value) {
     if (value = parseInt(value)) {
-        return value;
+        return parseInt(value);
     } else {
         if (value == 'A') {
             return 11;
@@ -68,22 +88,32 @@ function getCardName(card) {
     return card.suit[0].toUpperCase() + card.value;
 }
 
-function checkWinner() {
+function checkBlackjackWinner() {
+    winner  =  Object.keys(players).find(key => players.points === 21);
+}
 
-    const result = players.filter(player.points == 21);
-    console.log(result);
+function getWinner() {
+    let winPlayer
+    if (players.length == 1) {
+        winPlayer = players[0];
+        game.winner = winPlayer.name
+        return;
+    }
+    winPlayerkey = Object.keys(players).reduce((a, b) => players[a].points > players[b].points ? a : b);
+    game.winner = players[winPlayerkey].name
 }
 
 router.get('/start', async (req, res) => {
     const url = "https://pickatale-backend-case.herokuapp.com/shuffle";
     resetPlayers();
     cards = await main(url)
-    //console.log(cards)
+
     addPlayer('Annie');
     addPlayer('Bob');
     play();
-    console.log(players)
-    res.status(200).json({'cards': cards});   
+    if (!game['winner'])
+        getWinner()
+    res.status(200).json(game);   
 })
 
 module.exports = router
